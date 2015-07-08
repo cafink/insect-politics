428c56be3b0f4af05bc7a605948fca3cd156d551
Carl Fink <cafink@gmail.com>
2015-07-08 18:45:51 -0500

    Render Markdown as actual HTML.

--- include/templates/posts/view.php
+++ include/templates/posts/view.php
@@ -76,12 +76,13 @@
 
 		var Comment = React.createClass({
 			render: function() {
+				var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
 				return (
 					<div className="comment">
 						<h3 className="commentAuthor">
 							{this.props.author}
 						</h3>
-						{marked(this.props.children.toString())}
+						<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
 					</div>
 				);
 			}
