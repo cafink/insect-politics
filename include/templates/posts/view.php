0dc157ed180239d992978799d44e539d59f923e2
Carl Fink <cafink@gmail.com>
2015-07-08 18:13:40 -0500

    Build skeletons for CommentList and CommentForm.

--- include/templates/posts/view.php
+++ include/templates/posts/view.php
@@ -63,6 +63,26 @@
 <?php if ($GLOBALS['config']['show_comments']) { ?>
 	<div id="comments"></div>
 	<script type="text/jsx">
+		var CommentList = React.createClass({
+			render: function() {
+				return (
+					<div className="commentList">
+						Hello, world!  I am a CommentList.
+					</div>
+				);
+			}
+		});
+
+		var CommentForm = React.createClass({
+			render: function() {
+				return (
+					<div className="commentForm">
+					Hello, world!  I am a CommentForm.
+					</div>
+				);
+			}
+		})
+
 		var CommentBox = React.createClass({
 			render: function() {
 				return (
