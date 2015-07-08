0d6da830c659948e99a0f32054584058278ed0fd
Carl Fink <cafink@gmail.com>
2015-07-08 18:20:44 -0500

    Create the Comment component.

--- include/templates/posts/view.php
+++ include/templates/posts/view.php
@@ -73,6 +73,19 @@
 			}
 		});
 
+		var Comment = React.createClass({
+			render: function() {
+				return (
+					<div className="comment">
+						<h3 className="commentAuthor">
+							{this.props.author}
+						</h3>
+						{this.props.children}
+					</div>
+				);
+			}
+		});
+
 		var CommentForm = React.createClass({
 			render: function() {
 				return (
