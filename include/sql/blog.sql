67e3492191e3c93897d6cdbac48c4c2fbc776196
Carl Fink <cafink@gmail.com>
2016-08-11 22:29:03 -0500

    Import view counts from Blogger

--- include/sql/blog.sql
+++ include/sql/blog.sql
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
 	`title` varchar(255) NOT NULL,
 	`body` text NOT NULL,
 	`timestamp` timestamp NOT NULL,
+	`additional_views` int(11) NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
